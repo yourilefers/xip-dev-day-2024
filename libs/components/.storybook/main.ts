91a401dbba48e9541ab367d12bc265b376bfbcb8
Pepijn Yben <pepijn.yben@xip.nl>
2024-08-26 16:59:01 +0200

    fix: npm warnings and force node engine

--- libs/components/.storybook/main.ts
+++ libs/components/.storybook/main.ts
@@ -2,14 +2,17 @@ import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+
   addons: [
     '@storybook/addon-essentials', 
     '@storybook/addon-interactions'
   ],
+
   framework: {
     name: '@storybook/web-components-vite',
     options: {},
   },
+
   async viteFinal(config) {
     // Merge custom configuration into the default config
     const { mergeConfig } = await import('vite');
@@ -21,6 +24,8 @@ const config: StorybookConfig = {
       },
     });
   },
+
+  docs: {}
 };
 
 export default config;
